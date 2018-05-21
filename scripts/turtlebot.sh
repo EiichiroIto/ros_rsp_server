d37a799214793b5f2e53a13510c2c7941c6b9af9
Eiichiro Ito <e-itoh@ygu.ac.jp>
2018-05-21 16:06:21 +0900

    create launch file and delete shell scripts.

--- scripts/turtlebot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# author: Eiichiro Ito
-# e-mail: e-itoh@ygu.ac.jp
-# created: 2018/5/20
-# License: MIT License
-python ./turtlebot.py cmd_vel:=cmd_vel_mux/input/teleop bumper:=mobile_base/events/bumper cliff:=mobile_base/events/cliff
--- scripts/turtlesim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# author: Eiichiro Ito
-# e-mail: e-itoh@ygu.ac.jp
-# created: 2018/5/20
-# License: MIT License
-python ./turtlebot.py cmd_vel:=turtle1/cmd_vel
